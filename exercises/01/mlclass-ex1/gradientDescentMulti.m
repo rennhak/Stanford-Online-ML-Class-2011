2b08bbfba5d83dc16027f0ba7198ac1ad46e5bb1
Bjoern Rennhak <bjoern@rennhak.com>
2011-10-21 14:48:22 +0900

    - Added Gradient descent method with multiple variables

--- exercises/01/mlclass-ex1/gradientDescentMulti.m
+++ exercises/01/mlclass-ex1/gradientDescentMulti.m
@@ -17,20 +17,30 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+  theta_vals = theta;
+
+  for j = 1:length(theta)
+
+    cost_sum = 0;
+
+    for i = 1:m
+      cost_sum  = cost_sum + ( ( hypothesis( X, theta, i ) - y(i) ) * X(i,j) );
+    end % of for i
+
+    theta_vals(j) = theta(j) - ( alpha * (1/m) ) * cost_sum;
+  end % of for j
+
+  % Not sure if this is really necessary, need to learn octave language
+  % probably theta = theta_vals is enough.
+  for a = 1:length( theta )
+    theta( a ) = theta_vals( a );
+  end % of for a
 
 
+  % ============================================================
 
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
+  % Save the cost J in every iteration    
+  J_history(iter) = computeCostMulti(X, y, theta);
 
 end
 
