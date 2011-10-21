cef20924ec5f601b4323801f3af2669a1fa577c4
Bjoern Rennhak <bjoern@clothesnetwork.com>
2011-10-21 14:16:01 +0900

    - Finished Feature Normalization

--- exercises/01/mlclass-ex1/featureNormalize.m
+++ exercises/01/mlclass-ex1/featureNormalize.m
@@ -26,10 +26,35 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+m   = length( X );
+n   = length( X(1,:) );
+
+mu  = mean( X );
+
+% Substract the mean from the dataset
+% go from 1->down
+for i = 1:m
+  % go from 1->right
+  for j = 1:n
+    X(i,j) = X(i,j) - mu(j);
+  end % of for j
+end % of for i
+
+% Calculate the standard devation on the new data
+sigma = std( X );
+
+% compute standard deviation of each feature and divide
+% each feature by it's standard deviation
+% go from 1->down
+for i = 1:m
+  % go from 1->right
+  for j = 1:n
+    X(i,j) = X(i,j) / sigma(j);
+  end % of for j
+end % of for i
 
 
-
-
+X_norm = X;
 
 
 
