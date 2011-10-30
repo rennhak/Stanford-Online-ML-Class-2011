03b044d840012d8f733b4ac81a397be4901bdf45
Bjoern Rennhak <bjoern@rennhak.com>
2011-10-31 03:15:38 +0900

    - Added costfunction (has currently a bug)

--- exercises/02/mlclass-ex2/costFunction.m
+++ exercises/02/mlclass-ex2/costFunction.m
@@ -6,6 +6,7 @@ function [J, grad] = costFunction(theta, X, y)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length( X(1,:) );
 
 % You need to return the following variables correctly 
 J = 0;
@@ -21,10 +22,41 @@ grad = zeros(size(theta));
 %
 
 
+cost_sum  = 0;
 
+for i = 1:m
+  %a = theta'
+  %b = X( i, : )
+  %c = a .* b
+  hyp_val   = theta' .* X(i,:);
+end
 
+hyp       = sigmoid( hyp_val );
 
+for i = 1:m
+  t1        = -y(i) * log( hyp );
+  t2        = (1-y(i)) * log( 1 - hyp );
 
+  t         = t1 - t2;
+  cost_sum  += t;
+end
+
+J = (1/m) * cost_sum;
+
+grad_sum  = 0;
+n         = length( theta );
+
+for i = 1:m
+  for j = 1:n
+    t1        = hyp - y(i);
+    t2        = X(i,j);
+
+    t         = t1 .* t2;
+    grad_sum  += t;
+  end % of for j
+end % of for i
+
+grad = (1/m) * grad_sum;
 
 
 % =============================================================
